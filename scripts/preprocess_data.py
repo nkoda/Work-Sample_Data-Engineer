92fb3b2c24080e903eda4bded9eb236dc262796f
Nikko <dumriquenikko@gmail.com>
2023-05-01 16:38:48 -0700

    refactored the data persistence into a util directory

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,18 +1,13 @@
 import os
-import requests
 import pandas as pd
-import numpy as np
-import pyarrow as pa
+from util.data_handling import import_csv_as_df, export_df_as_parquet
 
 data_directory = '../data/'
 data_directory_output = 'processed'
 
-def import_data(directory, filename):
-    path = os.path.join(directory, filename)
-    schema = pa.csv.read_csv(path).schema
-    table = pa.csv.read_(path, schema=schema)
-    df = table.to_pandas()
-    df['Symbol'] = filename.replace('.csv', '')
+def import_data(directory, file_name):
+    df = import_csv_as_df(directory, file_name)
+    df['Symbol'] = file_name.replace('.csv', '')
     return df
 
 def combine_dir_data(path):
@@ -25,21 +20,13 @@ def combine_dir_data(path):
     result = pd.concat(dfs, ignore_index=True)
     return result
 
-def save_data(dataframe, filename):
-    file_path = os.path.join(
-        data_directory, 
-        data_directory_output, 
-        filename + ".parquet"
-        )
-    dataframe.to_parquet(file_path)
-
 if __name__ == '__main__':
     etfs_data_path = os.path.join(data_directory, 'raw', 'etfs')
     stocks_data_path = os.path.join(data_directory, 'raw', 'stocks')
     pd_etfs_data = combine_dir_data(etfs_data_path)
     pd_stocks_data = combine_dir_data(stocks_data_path)
     result = pd.concat([pd_etfs_data, pd_stocks_data], ignore_index=True)
-    save_data(result, 'processed_data')
+    export_df_as_parquet(result, 'processed', 'preprocessed_data')
 
 
 
