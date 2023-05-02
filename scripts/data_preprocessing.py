f7e2455f8da5da7fca78d22baf434b4c1612fa07
Nikko <dumriquenikko@gmail.com>
2023-05-01 17:53:03 -0700

    documented data_preprocessing

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -2,15 +2,35 @@ import os
 import pandas as pd
 from util.data_handling import import_csv_as_df, export_df_as_parquet
 
+# data paths for persistence
 data_directory = '../data/'
 data_preprocessing_output_path = os.path.join(data_directory, 'processed')
+etfs_data_path = os.path.join(data_directory, 'raw', 'etfs')
+stocks_data_path = os.path.join(data_directory, 'raw', 'stocks')
 
 def import_data(directory, file_name):
+    """Import data from a CSV file as a Pandas DataFrame.
+
+    Args:
+        directory (str): The directory where the CSV file is located.
+        file_name (str): The name of the CSV file to import.
+
+    Returns:
+        pandas.DataFrame: The DataFrame containing the imported data.
+    """
     df = import_csv_as_df(directory, file_name)
     df['Symbol'] = file_name.replace('.csv', '')
     return df
 
 def combine_dir_data(path):
+    """Combine data from all CSV files in a directory into a single DataFrame.
+
+    Args:
+        path (str): The directory containing the CSV files.
+
+    Returns:
+        pandas.DataFrame: The DataFrame containing the combined data.
+    """
     #load all data
     dfs = []
     for file in os.listdir(path):
@@ -21,14 +41,8 @@ def combine_dir_data(path):
     return result
 
 if __name__ == '__main__':
-    etfs_data_path = os.path.join(data_directory, 'raw', 'etfs')
-    stocks_data_path = os.path.join(data_directory, 'raw', 'stocks')
     pd_etfs_data = combine_dir_data(etfs_data_path)
     pd_stocks_data = combine_dir_data(stocks_data_path)
     result = pd.concat([pd_etfs_data, pd_stocks_data], ignore_index=True)
+    
     export_df_as_parquet(result, 'processed', 'preprocessed_data')
-
-
-
-
-
