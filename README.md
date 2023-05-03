0173dd8799501fd3216143707dd091735113fa36
Nikko <dumriquenikko@gmail.com>
2023-05-03 16:57:08 -0700

    added zipped data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Work-Sample_Data-Engineer
+version https://git-lfs.github.com/spec/v1
+oid sha256:944e83816fd70cae05dc4a6a79ff86d112eff81b8b540ec2a92dace401915598
+size 28
--- dags/data_pipeline.py
+++ dags/data_pipeline.py
@@ -1,26 +1,3 @@
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta
-
-from scripts.data_ingestion import ingest_data
-from scripts.data_augmentation import transform_data
-from scripts.train_model import deploy_model
-
-default_args = {
-    'owner': 'my_name',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 5, 3),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
-}
-
-dag = DAG('data_pipeline', default_args=default_args, schedule_interval='@monthly')
-
-# Define the tasks
-task_ingest = PythonOperator(task_id='ingest_data', python_callable=ingest_data, dag=dag)
-task_transform = PythonOperator(task_id='transform_data', python_callable=transform_data, dag=dag)
-taks_model_deployment = PythonOperator(task_id='deploy_model', python_callable=deploy_model, dag=dag)
-
-task_ingest >> task_transform >> taks_model_deployment
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee439df95a3bd98b89b04f3d7679c12ced17ac13832858cab90332fc641dc880
+size 955
