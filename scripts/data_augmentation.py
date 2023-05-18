974e004451005c5ba45e1c7bbff12a982ccba14b
Nikko <dumriquenikko@gmail.com>
2023-05-17 21:42:22 -0700

    implemented tests for data_augmentation task

--- scripts/data_augmentation.py
+++ scripts/data_augmentation.py
@@ -1,4 +1,8 @@
 import os
+import sys
+# Add the root directory to sys.path
+root_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(root_path)
 import logging
 import time
 import concurrent.futures
